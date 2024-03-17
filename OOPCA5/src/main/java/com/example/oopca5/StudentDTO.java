338da203b8a5f1a299b51686e328e9f48b56b19c
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2024-03-17 14:51:58 +0000

    Create StudentDTO.java
    The StudentDTO class is a Data Transfer Object (DTO) used to encapsulate data related to a student, such as their ID, student number, GPA, and name. This class is necessary for transferring student data between different layers of an application, such as between the data access layer ,is kind of constructor base file.

---
+++ OOPCA5/src/main/java/com/example/oopca5/StudentDTO.java
@@ -0,0 +1,61 @@
+public class StudentDTO {
+    private int id;
+    private int studentNumber;
+    private float gpa;
+    private String name;
+
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(int id, int studentNumber, float gpa, String name) {
+        this.id = id;
+        this.studentNumber = studentNumber;
+        this.gpa = gpa;
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "StudentDTO{" +
+                "id=" + id +
+                ", studentNumber=" + studentNumber +
+                ", gpa=" + gpa +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
