4b6154fc8a3bd41d15a9721811fec673049f529c
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2024-03-17 16:55:46 +0000

    Add files via upload
    The StudentDAOImpl class establishes a connection to the database using JDBC. It uses the DriverManager class to connect to a MySQL database hosted on localhost with the specified credentials.Within the getAllStudents() method, the StudentDAOImpl class executes a SQL query to fetch all student records from the "Student" table in the database. It then maps the retrieved data to StudentDTO objects and returns a list of these objects.And The getStudentById() method retrieves a specific student record from the database based on the provided ID.

---
+++ OOPCA5/src/main/java/com/example/oopca5/StudentDAOImpl.java
@@ -0,0 +1,54 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDAOImpl implements StudentDAO {
+    private Connection connection;
+
+    public StudentDAOImpl() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/OOPCA5", "root", "123");
+    }
+
+    @Override
+    public List<StudentDTO> getAllStudents() {
+        List<StudentDTO> students = new ArrayList<>();
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM Student")) {
+            while (rs.next()) {
+                StudentDTO student = new StudentDTO();
+                student.setId(rs.getInt("id"));
+                student.setStudentNumber(rs.getInt("studentNumber"));
+                student.setGpa(rs.getFloat("gpa"));
+                student.setName(rs.getString("name"));
+                students.add(student);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+
+    @Override
+    public StudentDTO getStudentById(int id) {
+        StudentDTO student = null;
+        try (PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Student WHERE id = ?")) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                student = new StudentDTO();
+                student.setId(rs.getInt("id"));
+                student.setStudentNumber(rs.getInt("studentNumber"));
+                student.setGpa(rs.getFloat("gpa"));
+                student.setName(rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return student;
+    }
+
+
+
+
+
+}
