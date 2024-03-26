98ac46ba0d6466be14d6a2f0c916def62006bcda
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2024-03-26 21:46:05 +0000

    Create JsonConverter.java
    Added a new class named JsonConverter to the project.
    This class provides methods to convert entities into JSON strings.
    
    Implemented a static method listEntitiesToJson to convert a list of entities into a JSON string.
    
    Implemented another static method entityToJson to convert a single entity into a JSON string.
    
    Used the Gson library for JSON serialization.
    
    These methods will be useful for serializing entity objects into JSON format, which can be sent over the network or stored in a file.

---
+++ OOPCA5/src/main/java/com/example/oopca5/JsonConverter.java
@@ -0,0 +1,17 @@
+import com.google.gson.Gson;
+
+import java.util.List;
+
+public class JsonConverter {
+    private static final Gson gson = new Gson();
+
+    // Feature 7: Convert List of Entities to a JSON String
+    public static <T> String listEntitiesToJson(List<T> list) {
+        return gson.toJson(list);
+    }
+
+    // Feature 8: Convert a single Entity by Key as a JSON String
+    public static <T> String entityToJson(T entity) {
+        return gson.toJson(entity);
+    }
+}
